57327ba8681d195d1af02b9f07cbc3c6af048756
nsmith <natesmith@ou.edu>
2020-01-31 15:20:15 -0600

    Adds detail view test.

--- reinhardt/tests/test_views.py
+++ reinhardt/tests/test_views.py
@@ -89,7 +89,7 @@ class TestViewActions(TestCase):
         self.assertContains(response, self.test_model_instance.test_field)
 
     def test_detail_view(self):
-        url = reverse('detail')
+        url = reverse('details')
         response = self.client.get(url)
         self.assertContains(response, TestDetailView().page_title)
         self.assertContains(response, self.test_model_instance.test_field)
